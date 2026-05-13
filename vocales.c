aca59f185aa318f1bec5477dec7046662f48373f
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-13 15:04:51 -0500

    3er commit: Modificación de la última vocal ('u' a 'x') usando un puntero auxiliar

--- vocales.c
+++ vocales.c
@@ -2,8 +2,10 @@
 
 int main (int argc, char *argv[]) {
     char vocales[5] = {'a', 'e', 'i', 'o', 'u'}; 
-    char *ptr;
+    char *ptr,*aux;
     ptr = &vocales[0];
+    aux = &vocales[4];
+    *aux = 'x';
     for (int i = 0; i < 5; i++) {
         // print de la forma clasica
         printf("vocales[%d] %c", i, vocales[i]);
