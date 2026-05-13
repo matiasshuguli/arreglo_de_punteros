41367fa6fffcd5cfd84e7a046a3f9d31806ef129
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-13 14:23:21 -0500

    2do commit: Implementación de impresión usando aritmética de punteros

--- main.c
+++ main.c
@@ -3,11 +3,20 @@
 int main() {
     
     int numeros[5] = {2, 4, 5, 8, 10};
+    int *ptr;
+    ptr = &numeros[0];
     
-    printf("Los numeros son: ");
-
+    // printf("%p",ptr);
+    
+    printf("Los numeros son: \n");
+    
+    // p es direccion, //d es el valor decimal
+    
     for (int i = 0; i < 5; i++) {
+        // print de la forma convencional
         printf("%d ", numeros[i]);
+        // print usando punteros
+        printf("%d\n",*(ptr+i));
     }
     return 0;
 }
