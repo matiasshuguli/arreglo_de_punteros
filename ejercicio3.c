1247d7daf24760143f68818c19851f5e1eccf014
Matias Shuguli <matias.shuguli@udla.edu.ec>
2026-05-13 15:18:59 -0500

    2do commit: Tabla completa de 8 tamaños de datos usando %zu

--- ejercicio3.c
+++ ejercicio3.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main (int argc, char *argv[]) {
-    char tab[] = {'a','e','i','o','u'};
-    //sizeof te dice cuantos espacios ocupa la palabra
-    int i = sizeof tab;
-    printf("%d",i);
+    printf("This computing environment uses:\n");
+    
+    printf("%zu byte  for chars\n", sizeof(char));
+    printf("%zu bytes for short ints\n", sizeof(short int));
+    printf("%zu bytes for ints\n", sizeof(int));
+    printf("%zu bytes for long ints\n", sizeof(long int));
+    printf("%zu bytes for long long ints\n", sizeof(long long int));
+    printf("%zu bytes for floats\n", sizeof(float));
+    printf("%zu bytes for doubles\n", sizeof(double));
+    printf("%zu bytes for pointers\n", sizeof(void*));
+    
     return 0;
 }
